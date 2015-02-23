b0d62f95767c69dd426d414ea2c3afe357a27de9
arthurmarks349 <arthurmarks349@gmail.com>
2015-02-23 09:30:33 -0500

    index.php created

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Coffee Club</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	  <link rel="stylesheet" href="css/main-custom.css">
+
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	  <script src="js/main-custom.js"></script>
+</head>
+
+<body>
+	  
+</body>
+</html>
